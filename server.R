243e7f688cf6c6381d948881689967f0751b1230
jyahn0103 <jyahn0103@gmail.com>
2019-12-30 10:38:36 +0900

    First filtering 기능, timeline 위치 변경 완료.
    Todo : 오른쪽 메뉴바 UI(넓이) 변경, 필터링 조건에 All 추가,

--- server.R
+++ server.R
@@ -1,54 +1,93 @@
 library(shiny)
+library(shinyjs)
 # Output: Histogram ----
 
 shinyServer(function(input,output, session){
+  
+  options(shiny.maxRequestSize=100*1024^2)
 
   step <- c()
-  dType <- c()
+  fType <- c()
   sample_num <- c()
   time <- c()
   
+  ############################
+  ##      RENDERING         ##
+  ############################
   
-  # output$data_type <- renderText({
-  #     input$data_type
-  # })
-  
-  output$input_data <- renderTable({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    
-    req(input$input_csv)
-    
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
-    tryCatch(
-      {
-        df <- read.csv(input$input_csv$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote)
-        
-        timeLine <- data.frame(step="Data Input",dType=input$data_type,sample_num=as.numeric(nrow(df)),time=as.character(Sys.time()))
-        print(timeLine)
-        #step <- c(step,"Data Input")
-        addTimeLine(timeLine)
-      },
-      error = function(e) {
-        # return a safeError if a parsing error occurs
-        stop(safeError(e))
-      }
-    )
-    
-    if(input$disp == "head") {
-      return(head(df))
+  observeEvent(input$file_upload_btn, {
+    if(input$file_upload_btn==0) {
+      shinyjs::hide("summary_box")
     }
-    else {
-      return(df)
+    shinyjs::show("summary_box")
+  })
+  
+
+  uploaded_data <- reactive({NULL})
+  
+  uploaded_data <- eventReactive(input$file_upload_btn, {
+    req(input$fileBrowser)
+    
+    if(is.null(input$fileBrowser)){
+      return(NULL)
+    }
+    tryCatch({
+      temp_data <- read.table(input$fileBrowser$datapath,
+                                  header = T,
+                                  fill = T,
+                                  sep = "\t")
+      
+      if(input$first_filtering == 1) {
+        temp_data <- dplyr::filter(temp_data, Potential.contaminant != "+")
+      } else if(input$first_filtering == 2) {
+        temp_data <- dplyr::filter(temp_data, Reverse != "+")
+      } else if(input$first_filtering == 3) {
+        temp_data <- dplyr::filter(temp_data, Only.identified.by.site != "+")
+      } else if(input$first_filtering == 4) {
+        temp_data <- temp_data
+      } 
+      
+      timeLine <- data.frame(step="Data Input",fType=input$file_type,
+                             sample_num=as.numeric(nrow(temp_data)),
+                             time=as.character(Sys.time()))
+      addTimeLine(timeLine)
+      
+    }, error=function(e){
+      stop(safeError(e))
+    })
+    
+    return(temp_data)
+  })
+  
+  
+  
+  observeEvent(input$file_upload_btn, {
+    if(is.null(input$first_filtering)){
+      shinyalert("Choose filetering option!", type="error", timer = 10000,
+                 closeOnClickOutside = T, closeOnEsc = T)
+    } else {
+      output$uploaded_file_header <- DT::renderDataTable({
+        uploaded_data()
+      }, options = list(scrollX = TRUE, pageLength = 5,lengthMenu = c(5, 10, 15)))
+      
+      print(nrow(uploaded_data()))
     }
     
   })
+  
+  observeEvent(input$first_filtering==4, {
+    print("ALL")
+    
+  })
+  
+  
+
+  
+
+
+  
+  
+  
   
   addTimeLine = function(timeLine){
     output$timeline <- renderUI({
@@ -60,16 +99,20 @@ shinyServer(function(input,output, session){
             #timelineLabel(timeLine$sample_num, color = "teal"),
             timelineItem(
               style="width:400px;",
-              title = timeLine$step,
+              title = paste0(timeLine$step),
               icon = "gears",
               color = "olive",
-              time = timeLine$time,
-              footer = paste0("Data Type : \n ", timeLine$dType)
+              # time = timeLine$time,
+              footer = paste0("File Type : \n ", timeLine$fType)
             )
           )
         }),
         timelineStart(color = "gray")
       )
     })
-  }
+  } # End of addTimeLine
+  
+  
+  
+  
 })
--- ui.R
+++ ui.R
@@ -1,84 +1,83 @@
 library(shiny)
 library(shinydashboard)
 library(shinydashboardPlus)
+library(shinyalert)
+
 #Edit 2019.12.26
 # Define UI for app that draws a histogram ----
-shinyUI(dashboardPagePlus(
-  dashboardHeaderPlus(
-    enable_rightsidebar = TRUE,
-    rightSidebarIcon = "clock"
-  ),
-  dashboardSidebar(
-    br(),
-    gradientBox(
-      title = "Input Data Type",
-      width = 12,
-      #icon = "fa fa-heart",
-      gradientColor = "maroon", 
-      boxToolSize = "md", 
-      closable = F,
-      footer= radioButtons("data_type", label="", 
-                           choices = list("LFQ" = "LFQ", "iBAQ" = "iBAQ", "TMT" = "TMT"), 
-                           selected = "LFQ")
+ui <- function(request) {shinyUI(
+  dashboardPagePlus(
+    dashboardHeaderPlus(
+      enable_rightsidebar = TRUE,
+      rightSidebarIcon = "sliders"
     ),
-    gradientBox(
-      title = "Input CSV Data",
-      width = 12,
-      #icon = "fa fa-heart",
-      gradientColor = "maroon", 
-      boxToolSize = "md", 
-      closable = F,
-      footer = fluidRow(
-        fileInput("input_csv", "Choose CSV File",
-                        multiple = FALSE,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")),
-        # Horizontal line ----
-        tags$hr(),
-        # Input: Checkbox if file has header ----
-        checkboxInput("header", "Header", TRUE),
-        # Input: Select separator ----
-        radioButtons("sep", "Separator",
-                     choices = c(Comma = ",",
-                                 Semicolon = ";",
-                                 Tab = "\t"),
-                     selected = ","),
-        # Input: Select quotes ----
-        radioButtons("quote", "Quote",
-                     choices = c(None = "",
-                                 "Double Quote" = '"',
-                                 "Single Quote" = "'"),
-                     selected = '"'),
-        # Horizontal line ----
-        tags$hr(),
-        # Input: Select number of rows to display ----
-        radioButtons("disp", "Display",
-                     choices = c(Head = "head",
-                                 All = "all"),
-                     selected = "head"),
-        tags$hr(),
-        checkboxGroupInput("first_filtering", label="First Filtering (Remove all)", 
-                     choices = list("Potential contaminant" = 1, "Reverse" = 2, "Only identified by site" = 3), 
-                     selected = T)
+    sidebar = dashboardSidebar(
+      sidebarMenu(
+        menuItem("TimeLine", selected = TRUE, icon=icon("clock"),
+                 tabName = "timeline", uiOutput("timeline"))
       )
-    )
-  ),
-  dashboardBody(
-    box(
-      width = NULL,
-      tableOutput("input_data")
-    )
-  ),
-  rightSidebar(
-    background = "dark",
-    rightSidebarTabContent(
-      id=1,
-      active = T,
-      icon="",
-      uiOutput("timeline")
-    )
-  ),
-  title = "DemoWEB"
+    ), # End of dashboardSidebar
+    body = dashboardBody(
+      box(
+        id = "summary_box",
+        solidHeader = T,
+        width = 10,
+        # tableOutput("uploaded_file_header")
+        DT::dataTableOutput("uploaded_file_header")
+      ) # End of uploaded file data table
+      
+    ), # End of dashboardBody
+    rightsidebar = rightSidebar(
+      useShinyalert(),
+      id = "rightsidebar",
+      width = 350,
+      background = "dark",
+      rightSidebarTabContent(
+        id=1,
+        active = T,
+        icon="desktop",
+        
+        gradientBox(
+          title = "Upload Data",
+          width = 12,
+          gradientColor = "maroon", 
+          boxToolSize = "md", 
+          closable = F,
+          footer = fluidRow(
+            fileInput("fileBrowser", "Choose txt File",
+                      multiple = FALSE,
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv")),
+            tags$hr(),
+            radioButtons("file_type", label="", 
+                         choices = list("LFQ" = "LFQ", "iBAQ" = "iBAQ", "TMT" = "TMT"), 
+                         selected = "LFQ"),
+            
+            checkboxGroupInput("first_filtering", label="First Filtering (Remove all)", 
+                               choices = list("Potential contaminant" = 1, 
+                                              "Reverse" = 2, "Only identified by site" = 3,
+                                              "Without Filtering" = 4)) 
+            
+            
+          )
+        ), # End of Upload Data box
+        actionButton("file_upload_btn", "Upload file")
+      ),
+      rightSidebarTabContent(
+        id=2,
+        title = "Input File Format2",
+        icon="clock",
+        textInput("caption", "Caption", "Data Summary")
+      ),
+      rightSidebarTabContent(
+        id=3,
+        title = "Input File Format3",
+        icon="gears",
+        textInput("caption", "Caption", "Data Summary")
+      )
+    ), # End of rightSidebar
+    title = "DemoWEB"
   )
-)
+)}
+  
